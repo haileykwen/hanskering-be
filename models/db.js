3d44c8b41b2d95b9fdd63f1b03cb52920311b594
haileykwen <anonyhailey@gmail.com>
2021-11-13 18:38:17 +0700

    setup env

--- models/db.js
+++ models/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
+require('dotenv').config();
 
 const db = mysql.createPool({
-    host: 'us-cdbr-east-04.cleardb.com',
-    user: 'b12baeae7407ec',
-    password: 'a0ab0db4',
-    database: 'heroku_3cbda1abf527e49'
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 })
 
 module.exports = db;
