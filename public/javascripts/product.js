8728405fac98fffcee57aa40c72f62c5be7cbdf6
haileykwen <anonyhailey@gmail.com>
2021-11-27 15:55:30 +0700

    implement table product and pagination

--- public/javascripts/product.js
+++ public/javascripts/product.js
@@ -1,5 +1,17 @@
 const btnCreate     = document.getElementById('btn-create');
+const pageInput     = document.getElementById('page-input');
 
 btnCreate.addEventListener('click', () => {
     window.location.href = '/cms/app/product/create';
+});
+
+pageInput.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        if ( pageInput.value ) {
+            const page = pageInput.value;
+            const target = `${window.location.origin}/cms/app/product?page=9`;
+            console.log(target);
+            window.location.href = `${window.location.origin}/cms/app/product?page=9`;
+        }
+    }
 });
--- routes/apiProductRoute.js
+++ routes/apiProductRoute.js
@@ -1,7 +1,9 @@
 const express               = require('express');
 const router                = express.Router();
-const { post_create }       = require("../controllers/apiProductController");
+const { post_create, get_all, get_product_pagination }       = require("../controllers/apiProductController");
 
 router.post('/create', post_create);
+router.get('/view-all', get_all);
+router.get('/pagination', get_product_pagination);
 
 module.exports = router;
