fa8aaf288f42995c82e52b6d622d71a997e1efa1
haileykwen <anonyhailey@gmail.com>
2021-12-01 01:51:23 +0700

    implement update product

--- routes/cmsAppRoute.js
+++ routes/cmsAppRoute.js
@@ -4,11 +4,13 @@ const {
     get_dashboard, 
     get_product, 
     get_createProduct, 
-    get_viewProduct}     = require('../controllers/cmsAppController');
+    get_viewProduct,
+    get_updateProduct }     = require('../controllers/cmsAppController');
 
 router.get('/dashboard', get_dashboard);
 router.get('/product', get_product);
 router.get('/product/create', get_createProduct);
 router.get('/product/view', get_viewProduct);
+router.get('/product/update', get_updateProduct);
 
 module.exports = router;
